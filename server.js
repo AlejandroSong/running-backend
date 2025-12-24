8b13cd1a9b5a3c8dcaa7d842f75e5933d507b328
Alejandro <davidalejandroroblesmarquez@gmail.com>
2025-12-24 14:29:16 -0600

    Forzando subida de Mongo

--- server.js
+++ server.js
@@ -125,3 +125,4 @@ io.on('connection', (socket) => {
 
 const PORT = process.env.PORT || 3000;
 server.listen(PORT, '0.0.0.0', () => console.log(`🚀 SERVIDOR LISTO: ${PORT}`));
+// Actualización forzada para MongoDB
